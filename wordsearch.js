600846188b13188982855047686001e50f86e435
Muhammed Tariq <momotrq94@gmail.com>
2022-04-13 14:33:09 -0400

    fixed word search to search horizontally, vertically and in reverse for both directions as well

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
+const transpose = function(matrix) {
+   let newMatrix = [];
+   
+   for (let i = 0; i < matrix[0].length; i++) {
+     let arr = [];
+     for (let j = 0; j < matrix.length; j++) {
+      arr.push(matrix[j][i]);
+     }
+     newMatrix.push(arr);
+   }
+   return newMatrix;
+  };
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    const horizontalReverseJoin = letters.map(ls => ls.reverse().join(''));
+    const verticalReverseJoin = transpose(letters).map(ls => ls.reverse().join(''));
+    //console.log("horR: ", horizontalReverseJoin);
+    //console.log("verR: ", verticalReverseJoin);
+    //console.log("hor: ", horizontalJoin);
+    //console.log("ver: ", verticalJoin);
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) return true;
     }
+    for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    for (let l of horizontalReverseJoin) {
+        if (l.includes(word)) return true;
+    }
+    for (let l of verticalReverseJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
